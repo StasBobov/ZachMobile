992f59a971895c420c54c0a4f796146e606a2599
stas <chousetheone@gmail.com>
2023-02-24 21:34:52 +0200

    Create a login function

--- my_base.py
+++ my_base.py
@@ -1,7 +1,12 @@
+from pprint import pprint
+
 import requests
 import json
 from kivy.app import App
 import logging
+import event_calendar
+import constants
+import pyrebase
 
 log = logging.getLogger('base_loger')
 log.setLevel(logging.DEBUG)
@@ -11,12 +16,30 @@ fh.setFormatter(formatter)
 log.addHandler(fh)
 
 
+
+
+
+firebaseConfig = {
+  "apiKey": "AIzaSyB1AH3rKpOY31O8IvhkIIi7rL0oB-o8bsE",
+  "authDomain": "zach-mobile.firebaseapp.com",
+  "databaseURL": "https://zach-mobile-default-rtdb.firebaseio.com",
+  "projectId": "zach-mobile",
+  "storageBucket": "zach-mobile.appspot.com",
+  "messagingSenderId": "1044564107596",
+  "appId": "1:1044564107596:web:42695e4d6711eb5d7b2fe9",
+  "measurementId": "G-2VEJTJQ2E0"
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+
+auth = firebase.auth()
+
+
 class MyBase:
     wak = "AIzaSyB1AH3rKpOY31O8IvhkIIi7rL0oB-o8bsE"
 
     # При нажатии на кнопку Sign up
     def sign_up(self, email, password):
-        print('Sign Up')
         app = App.get_running_app()
         signup_url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + self.wak
         signup_payload = {"email": email, "password": password, "returnSecureToken": True}
@@ -40,7 +63,7 @@ class MyBase:
             post_request = requests.patch("https://zach-mobile-default-rtdb.firebaseio.com/" + localId + ".json?auth="
                                           + idToken, data=my_data)
 
-            app.events_filling(sort=None)
+            event_calendar.events_filling(sort=None)
 
             app.change_screen('home_screen')
 
@@ -49,6 +72,25 @@ class MyBase:
             error_data = json.loads(sign_up_request.content.decode())
             error_message = error_data['error']['message']
             app.root.ids['login_screen'].ids['login_message'].text = error_message
+
+    def login(self, email, password):
+        app = App.get_running_app()
+        try:
+            login = auth.sign_in_with_email_and_password(email, password)
+            refresh_token = login['refreshToken']
+            localId = login['localId']
+            idToken = login['idToken']  # authToken
+            with open('refresh_token.txt', 'w') as f:
+                f.write(refresh_token)
+            app.local_id = constants.LOCAL_ID = localId  # uid
+            app.id_token = constants.ID_TOKEN = idToken
+            event_calendar.events_filling(sort=None)
+
+            app.change_screen('home_screen')
+        except Exception as ex:
+            error_dict = json.loads(ex.args[1])
+            app.root.ids['login_screen'].ids['login_message'].text = error_dict['error']['message']
+
 
     def exchange_refresh_token(self, refresh_token):
         refresh_url = 'https://securetoken.googleapis.com/v1/token?key=' + self.wak
